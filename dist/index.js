f5cfe4ac1f7c5776ab7cf069d32cd3e5df7f7169
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-09-04 18:08:21 +0000

    fix: generate a new build with the new settings [#2]

---
+++ dist/index.js
@@ -0,0 +1,54 @@
+/**
+ * @description Automate release processes using `semantic-release`
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ * @link        https://semantic-release.gitbook.io/semantic-release/
+ * @link        https://semantic-release.gitbook.io/semantic-release/developer-guide/js-api
+ */
+import semanticRelease from 'semantic-release';
+import * as core from '@actions/core';
+/**
+ * Action entry point
+ * @returns {Promise<void>} Resolves when the action is completed
+ */
+export async function run() {
+    try {
+        /**
+         * @note Dispatch release
+         */
+        const result = await semanticRelease({
+            branches: ['main'],
+            tagFormat: '${version}',
+            plugins: [
+                '@semantic-release/commit-analyzer',
+                '@semantic-release/release-notes-generator',
+                '@semantic-release/changelog',
+                '@semantic-release/npm',
+                '@semantic-release/git'
+            ]
+        });
+        /**
+         * @note Check release
+         */
+        if (result) {
+            const { commits, nextRelease } = result;
+            core.info(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`);
+        }
+        else {
+            core.info('No release published.');
+        }
+    }
+    catch (error) {
+        /**
+         * @note Fail the workflow run if an error occurs
+         */
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+    }
+}
+/**
+ * @note Execute action
+ */
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+run();
+//# sourceMappingURL=index.js.map
